768e4e14275ab79e2a2e7c57e200116673e64e26
Mathius <info@yogimathius.dev>
2024-04-01 23:33:04 -0700

    chore: add my_mouse.h

---
+++ inc/my_mouse.h
@@ -0,0 +1,11 @@
+#ifndef __MY_MOUSE_
+#define __MY_MOUSE_
+#include <stdio.h>
+#include <stdlib.h> 
+#include <fcntl.h>
+
+#define HELP "Usage: ./my_mouse [file]\n"
+
+int my_mouse(const char* file);
+
+#endif
