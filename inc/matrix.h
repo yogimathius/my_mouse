3800fcd965821707d6caa7996cab4f5583ee3fe8
Mathius <info@yogimathius.dev>
2024-04-14 21:23:39 -0700

    refactor: move matrix structs to matrix.h

---
+++ inc/matrix.h
@@ -0,0 +1,37 @@
+#ifndef __MATRIX_H_
+#define __MATRIX_H_
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define SIZE 16
+#define __MatrixClass_self_alignement 0xC0
+
+struct Matrix {
+    ssize_t size;
+    char* buffer;
+    int** matrix;
+    int rows;
+    int cols;
+    int max_val;
+    int max_row;
+    int max_col;
+    int (*read)(const char* filename);
+    int (*get_size)();
+    void (*alloc)();
+    int (*min_distance)();
+    void (*debug)();
+    void (*print)();
+    void (*free)();
+};
+
+
+extern const struct MatrixClass {
+    struct Matrix(*new)();
+} Matrix;
+
+#endif
--- inc/my_mouse.h
+++ inc/my_mouse.h
@@ -10,32 +10,6 @@
 
 #define HELP "Usage: ./my_mouse [file]\n"
 
-#define SIZE 16
-#define __MatrixClass_self_alignement 0xC0
-
-struct Matrix {
-    ssize_t size;
-    char* buffer;
-    int** matrix;
-    int rows;
-    int cols;
-    int max_val;
-    int max_row;
-    int max_col;
-    int (*read)(const char* filename);
-    int (*get_size)();
-    void (*alloc)();
-    void (*build)();
-    void (*debug)();
-    void (*print)();
-    void (*free)();
-};
-
-
-extern const struct MatrixClass {
-    struct Matrix(*new)();
-} Matrix;
-
 int my_mouse(const char* file);
 
 #endif
